47e2a5b12fa40471a3a2b756826d69192bec6b32
Diogo Cercilio <diogocercilio@gmail.com>
2020-03-10 01:36:32 -0300

    Updates

---
+++ precache-manifest.2fb741ead17bbfd5b36cd94ab847f249.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1b1204721d6c70e92186cf33c5cad643",
+    "url": "/index.html"
+  },
+  {
+    "revision": "dc138572ec02e5dd3078",
+    "url": "/static/css/main.f69bda67.chunk.css"
+  },
+  {
+    "revision": "1ce799d97820d6c64db5",
+    "url": "/static/js/2.f67d3f4d.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/static/js/2.f67d3f4d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dc138572ec02e5dd3078",
+    "url": "/static/js/main.34cb796a.chunk.js"
+  },
+  {
+    "revision": "a27870e5f03a3425f6aa",
+    "url": "/static/js/runtime-main.c70048c8.js"
+  }
+]);
