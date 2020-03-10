e9cd6ff5de01be7811c72111ab7d559544e1611a
Diogo Cercilio <diogocercilio@gmail.com>
2020-03-10 01:45:00 -0300

    Fix menu anchor

--- src/components/menu/menu-sidebar/MenuSidebarItem.js
+++ src/components/menu/menu-sidebar/MenuSidebarItem.js
@@ -4,7 +4,7 @@ import './MenuSidebarItem.scss';
 export default function ItemMenuSidebar({ item, itemActive, onChangeMenu }) {
     return (
         <li className={`MenuSidebarItem${itemActive === item.id ? ' active' : ''}`} onClick={()=> { onChangeMenu(item.id) }}>
-            <a href={`#${item.alias}`}>
+            <a href={`#${item.alias}`} onClick={e=> e.preventDefault()}>
                 {item.name}
             </a>
         </li>
