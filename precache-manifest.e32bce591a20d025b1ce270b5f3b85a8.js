6d08012d82fe6bffc97a37a90ec0fbbad3d19173
Diogo Silva <diogocercilio@gmail.com>
2023-01-30 16:14:52 -0300

    Updates

---
+++ precache-manifest.e32bce591a20d025b1ce270b5f3b85a8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "34ec7fbc7c8554f7d673702fc0c979bc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "dd1cfea12f226d3ba4b5",
+    "url": "/static/css/2.e9597d0c.chunk.css"
+  },
+  {
+    "revision": "9a0ab449360f742f1310",
+    "url": "/static/css/main.8917bf6e.chunk.css"
+  },
+  {
+    "revision": "dd1cfea12f226d3ba4b5",
+    "url": "/static/js/2.142ecf35.chunk.js"
+  },
+  {
+    "revision": "0f2be52aac348a57b5e1722f475cbfd0",
+    "url": "/static/js/2.142ecf35.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9a0ab449360f742f1310",
+    "url": "/static/js/main.23a9a9d8.chunk.js"
+  },
+  {
+    "revision": "0d46f7656c748826843c",
+    "url": "/static/js/runtime-main.c2b20a0b.js"
+  }
+]);
