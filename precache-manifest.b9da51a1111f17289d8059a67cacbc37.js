4a66b84358a1e9828a0ad101e4e41717572276f4
Diogo Silva <diogocercilio@gmail.com>
2023-01-30 17:54:04 -0300

    Updates

---
+++ precache-manifest.b9da51a1111f17289d8059a67cacbc37.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7f4792868d033a90d86330f646896b22",
+    "url": "/DiogoCercilio.github.io/index.html"
+  },
+  {
+    "revision": "28040fad948728e405f4",
+    "url": "/DiogoCercilio.github.io/static/css/2.0e03a90d.chunk.css"
+  },
+  {
+    "revision": "c1766c35a47e8fe78990",
+    "url": "/DiogoCercilio.github.io/static/css/main.d4344a44.chunk.css"
+  },
+  {
+    "revision": "28040fad948728e405f4",
+    "url": "/DiogoCercilio.github.io/static/js/2.72cb6232.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/DiogoCercilio.github.io/static/js/2.72cb6232.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c1766c35a47e8fe78990",
+    "url": "/DiogoCercilio.github.io/static/js/main.003009ab.chunk.js"
+  },
+  {
+    "revision": "4e0e34f265fae8f33b01b27ae29d9d6f",
+    "url": "/DiogoCercilio.github.io/static/js/main.003009ab.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "801b29d0c353a7cb8c46",
+    "url": "/DiogoCercilio.github.io/static/js/runtime-main.4b1f6076.js"
+  }
+]);
